14e34b41acad9d3e53737beab77723237589f720
Pahalawaththage P. W. I. H. IT24101303 <it24101303@my.sliit.lk>
2026-04-24 10:57:41 +0530

    Updated API URL

--- src/api/itemApi.js
+++ src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://wmtbackend-production-a85a.up.railway.app/api",
 });
 
 export const getItems = () => API.get("/items");
